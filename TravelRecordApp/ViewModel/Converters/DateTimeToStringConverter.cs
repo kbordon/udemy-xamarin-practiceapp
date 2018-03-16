5e00350205381c6b6b31b8c292bd951830a1aabf
Kimberly Bordon <kbordon@gmail.com>
2018-03-15 22:23:06 -0700

    Create DateTimeToStringConverter

---
+++ TravelRecordApp/ViewModel/Converters/DateTimeToStringConverter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace TravelRecordApp.ViewModel.Converters
+{
+    public class DateTimeToStringConverter : IValueConverter
+    {
+        public DateTimeToStringConverter()
+        {
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            DateTimeOffset dateTime = (DateTimeOffset)value;
+            DateTimeOffset rightNow = DateTimeOffset.Now;
+            var difference = rightNow - dateTime;
+
+            if (difference.TotalDays > 1)
+                return $"{dateTime:d}";
+            else
+            {
+                if (difference.TotalSeconds < 60)
+                    return $"{difference.TotalSeconds} seconds ago";
+                if (difference.TotalMinutes < 60)
+                    return $"{difference.TotalMinutes} minutes ago";
+                if (difference.TotalHours < 24)
+                    return $"{difference.TotalHours} hours ago";
+
+                return "yesterday";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
